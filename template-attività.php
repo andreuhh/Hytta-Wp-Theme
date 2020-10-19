05c14b35ddeed007303f51bfad58eb22d8a0e774
Andrea Guffi <andreaguffi92@gmail.com>
2020-10-19 16:53:08 +0200

    modified search bar in activity & rental page

--- template-attività.php
+++ template-attività.php
@@ -8,6 +8,15 @@
     <div class="main__carddiv">
 
         <h2>Attività</h2>
+
+        <form role="search" method="get" class="search-form search-template" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentysixteen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+            <button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'twentysixteen' ); ?></span></button>
+            <?php if( class_exists('WooCommerce')): ?>
+                <input type="hidden" value="product" name="post_type" id="post_type">
+            <?php endif;?>
+        </form>
+
         <div class="container">                             
             <?php echo do_shortcode('[products limit="12" columns="4" orderby="rating" category="attività"]' ); ?>    
         </div>  
--- template-noleggio.php
+++ template-noleggio.php
@@ -2,18 +2,34 @@
    Template Name: noleggio
 -->
 <?php get_header()?>
-<main>
 
+
+
+
+
+<main>
 
     <div class="main__carddiv">
 
         <h2>Noleggio Attrezzatura</h2>
+
+        <form role="search" method="get" class="search-form search-template" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+            <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentysixteen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+            <button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'twentysixteen' ); ?></span></button>
+            <?php if( class_exists('WooCommerce')): ?>
+                <input type="hidden" value="product" name="post_type" id="post_type">
+            <?php endif;?>
+        </form>
+
+
         <div class="container">                             
             <?php echo do_shortcode('[products limit="8" columns="4" orderby="rating" category="noleggio"]' ); ?>    
         </div>  
                 
     </div>
 
-    </main>
+</main>
+
+
 
 <?php get_footer()?>
