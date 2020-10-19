1d7f28c1da44b63bcf8730034476da544ab305c6
Andrea Guffi <andreaguffi92@gmail.com>
2020-10-19 11:15:34 +0200

    style single prod, added link in home

--- template-home.php
+++ template-home.php
@@ -68,41 +68,56 @@ get_header();
         <div class="carousel-container">
             <div class="carousel-inner">
                 <div class="track">
-                    <div class="card-container-carousel">
-                        <div class="card-carousel">
-                            <div class="img-carousel img-1" alt="water sport picture">
-                                <p>Acqua</p>
+                    <a href="<?php echo site_url('/tag-prodotto/acqua/') ?>">
+                        <div class="card-container-carousel">
+                            <div class="card-carousel">
+                                <div class="img-carousel img-1" alt="water sport picture">
+                                    <p>Acqua</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card-container-carousel">
-                        <div class="card-carousel">
-                            <div class="img-carousel img-2" alt="bmx sport picture">
-                                <p>Terra</p>
+                    </a>
+
+                    <a href="<?php echo site_url('/tag-prodotto/terra/') ?>">
+                        <div class="card-container-carousel">
+                            <div class="card-carousel">
+                                <div class="img-carousel img-2" alt="bmx sport picture">
+                                    <p>Terra</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card-container-carousel">
-                        <div class="card-carousel">
-                            <div class="img-carousel img-3" alt="zipline sport picture">
-                                <p>Aria</p>
+                    </a>
+
+                    <a href="<?php echo site_url('/tag-prodotto/aria/') ?>">
+                        <div class="card-container-carousel">
+                            <div class="card-carousel">
+                                <div class="img-carousel img-3" alt="zipline sport picture">
+                                    <p>Aria</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card-container-carousel">
-                        <div class="card-carousel">
-                            <div class="img-carousel img-4" alt="skii sport picture">
-                                <p>Neve</p>
+                    </a>
+
+                    <a href="<?php echo site_url('/tag-prodotto/neve/') ?>">
+                        <div class="card-container-carousel">
+                            <div class="card-carousel">
+                                <div class="img-carousel img-4" alt="skii sport picture">
+                                    <p>Neve</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="card-container-carousel">
-                        <div class="card-carousel">
-                            <div class="img-carousel img-5" alt="skateboard sport picture">
-                                <p>Urban</p>
+                    </a>
+
+
+                    <a href="<?php echo site_url('/tag-prodotto/urban/') ?>">
+                        <div class="card-container-carousel">
+                            <div class="card-carousel">
+                                <div class="img-carousel img-5" alt="skateboard sport picture">
+                                    <p>Urban</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                     
                 </div>
             </div>
@@ -121,118 +136,20 @@ get_header();
 
         <div class="main__carddiv">
             <h2>Attività outdoor in evidenza</h2>
-            <div class="container">
-                <section class="cards">
-                    
-                        <article>
-                            <a href="singleActivity.html">
-                              <img src="img/card/horse1-card.jpg" alt="horse riding activity">
-                            </a>
-                            <div class="text">
-                                <a href="singleActivity.html">
-                                 <h4>Giornata a cavallo sui colli della Maremma</h4>
-                                </a>
-                                <p>Attività'</p>
-                                <p>Categoria: Equitazione</p>
-                                <p><i class="fas fa-map-marker-alt"></i><span>  Montieri, Grosseto</span></p>
-                                <p><i class="far fa-clock"></i><span>  Durata: 6 ore</span></p>
-                                <p>da<span class="orange"> 90€</span></p>
-                            </div>
-                        </article>
-                   
-    
-                    <article>
-                        <img src="img/card/rafting2-card.jpg" alt="rafting activity">
-                        <div class="text">
-                            <h4>Rafting Marathon sul fiume Noce in Trentino</h4>
-                            <p>Attività'</p>
-                            <p>Categoria: Rafting</p>
-                            <p><i class="fas fa-map-marker-alt"></i><span>  Ossana, Trento</span></p>
-                            <p><i class="far fa-clock"></i><span> 4 ore</span></p>
-                            <p>da<span class="orange"> 64€</span></p>
-                        </div>
-                    </article>
-    
-                    <article>
-                        <img src="img/card/surfclass1-card.jpg" alt="surfing course activity">
-                        <div class="text">
-                            <h4>Corso privato di surf al lido di Ostia</h4>
-                            <p>Attività'</p>
-                            <p>Categoria: Surf</p>
-                            <p><i class="fas fa-map-marker-alt"></i><span> Lido di Ostia</span></p>
-                            <p><i class="far fa-clock"></i><span> 2 ore</span></p>
-                            <p>da<span class="orange"> 30€</span></p>
-                        </div>
-                    </article>
-    
-                    <article>
-                        <img src="img/card/yoga1-card.jpg" alt="yoga course">
-                        <div class="text">
-                            <h4>Lezione privata Yoga e Meditazione a Canazei</h4>
-                            <p>ATTIVITA'</p>
-                            <p>Yoga e Meditazione</p>
-                            <p><i class="fas fa-map-marker-alt"></i><span> Canazei, Trento</span></p>
-                            <p><i class="far fa-clock"></i><span> 2 ore</span></p>
-                            <p>da<span class="orange"> 50€</span></p>
-                        </div>
-                    </article>
-                </section>
+            <div class="container">                             
+                <?php echo do_shortcode('[products limit="4" columns="4" orderby="popularity" category="attività"]' ); ?>    
             </div>  
             
-            <a href="attivita.html"><button class="btn-secondary" type="button">Vedi altre</button></a>
+            <a href="<?php echo site_url('/attivita') ?>"><button class="btn-secondary" type="button">Vedi altre</button></a>
         </div>
 
         <div class="main__carddiv">
             <h2>Noleggio: attrezzatura in evidenza</h2>
             <div class="container">
-                <section class="cards">
-                    <article>
-                        <img src="img/card/EBike1-card.jpg" alt="E-bike photo">
-                        <div class="text">
-                            <h4>JEEP E-bike</h4>
-                            <p>Noleggio</p>
-                            <p>Categoria: Biciclette</p>
-                            <p><i class="fas fa-map-marker-alt"></i><span> Luogo</span></p>
-                            <p>da<span class="orange"> 20€</span> (Orario)</p>
-                        </div>
-                    </article>
-    
-                    <article>
-                        <img src="img/card/skateRent1-card.jpg" alt="Skateboard photo">
-                        <div class="text">
-                            <h4>Longboard professionale</h4>
-                            <p>Noleggio</p>
-                            <p>Categoria: Tavole e skate</p>
-                            <p><i class="fas fa-map-marker-alt"></i><span> Luogo: Milano</span></p>
-                            <p>da<span class="orange"> 20€</span> (Giornaliero)</p>
-                        </div>
-                    </article>
-    
-                    <article>
-                        <img src="img/card/kayakRent1-card.jpg" alt="Kayak picture">
-                        <div class="text">
-                            <h4>Kayak aperto individuale</h4>
-                            <p>Noleggio</p>
-                            <p>Categoria'</p>
-                            <p><i class="fas fa-map-marker-alt"></i><span> Luogo</span></p>
-                            <p>da<span class="orange"> 15€ </span>(Orario)</p>
-                        </div>
-                    </article>
-    
-                    <article>
-                        <img src="img/card/bikeGravel1.jpg" alt="Gravel bike picture">
-                        <div class="text">
-                            <h4>Bici da Corsa</h4>
-                            <p>Noleggio</p>
-                            <p>Categoria: Biciclette</p>
-                            <p><i class="fas fa-map-marker-alt"></i><span> Luogo:</span> Firenze</p>
-                            <p>da<span class="orange"> 35€</span> (Giornaliero)</p>
-                        </div>
-                    </article>
-                </section>
+                <?php echo do_shortcode('[products limit="4" columns="4" orderby="date" category="noleggio"]' ); ?>   
             </div>  
             
-            <a href="noleggio.html">
+            <a href="<?php echo site_url('/noleggio') ?>">
                 <button class="btn-secondary" type="button">Vedi altre</button>
             </a>
             
@@ -257,7 +174,7 @@ get_header();
         <h2>Le Attività preferite dai nostri utenti</h2>
         <div class="containerGallery">
             <div class="containerGallery__item1" alt="canyoning activity">
-                <h5>Canyoning in Valsella</h5>
+                <h5>Canyoning in Valle d'Aosta</h5>
                 <div class="containerGallery__price">
                     <p>da <span>50€</span></p>
                 </div>
